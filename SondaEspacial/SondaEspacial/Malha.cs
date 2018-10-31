3449efd9e34528dbbf2cee21e83a97c01a22f62e
Gustavo Matos <37024361+Toic@users.noreply.github.com>
2018-10-31 16:48:24 -0300

    Criação de novo método

--- SondaEspacial/SondaEspacial/Malha.cs
+++ SondaEspacial/SondaEspacial/Malha.cs
@@ -8,8 +8,6 @@ namespace SondaEspacial
 {
     public class Malha
     {
-        private Coordenada[,] posicoes { get; set; }
-
         public Malha(int coordenadaMaxX, int coordenadaMaxY)
         {
             if (coordenadaMaxX < 1) { throw new ArgumentException("Coordenada X não pode ser menor que 1"); }
@@ -17,17 +15,17 @@ namespace SondaEspacial
 
             this.CoordenadaMaxX = coordenadaMaxX;
             this.CoordenadaMaxY = coordenadaMaxY;
-            posicoes = new Coordenada[coordenadaMaxX, coordenadaMaxY];
+            Posicoes = new Coordenada[coordenadaMaxX, coordenadaMaxY];
 
             for (int i = 0; i <= CoordenadaMaxX; i++)
             {
                 for (int j = 0; j <= CoordenadaMaxY; j++)
                 {
-                    posicoes[i, j] = new Coordenada(i, j);
+                    Posicoes[i, j] = new Coordenada(i, j);
                 }
             }
         }
-
+        private Coordenada[,] Posicoes { get; set; }
         public int CoordenadaMaxX { get; private set; }
         public int CoordenadaMaxY { get; private set; }
 
@@ -38,7 +36,7 @@ namespace SondaEspacial
             {
                 for (int j = 0; j <= CoordenadaMaxY; j++)
                 {
-                    if (posicoes[i, j].EstaOcupado) { estaVazia = false; }
+                    if (Posicoes[i, j].EstaOcupado) { estaVazia = false; }
                 }
             }
             return estaVazia;
@@ -46,7 +44,10 @@ namespace SondaEspacial
 
         public void AdicionaSonda(Sonda sonda)
         {
-
+            if (PosicaoEstaVazia(sonda.PosicaoX, sonda.PosicaoY))
+            {
+                Posicoes[sonda.PosicaoX, sonda.PosicaoY].Sonda = sonda;
+            }
         }
     }
 }
